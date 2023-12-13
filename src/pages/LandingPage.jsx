5c11127c8a7d99294057c564a769d876efce33dd
Melanie Voloshynska <71847995+Melania00@users.noreply.github.com>
2023-12-14 01:14:52 +0200

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,40 +1,36 @@
 import React, { useState } from "react";
-import Description from '../components/Description';
-import Cart from '../components/Cart';
-
+import Description from "../components/Description";
+import Cart from "../components/Cart";
 
 const LandingPage = () => {
-    const [numberOfCarts, setNumberOfCarts] = useState(3);
-    return (
-        <main className="p-5">
-            <Description />
-            <section className="flex mb-8 mt-10">
-            {Array.from({ length: numberOfCarts }).map((_, index) => (
-                <Cart key={index} />
-            ))}
-            </section>
+  const [numberOfCarts, setNumberOfCarts] = useState(3);
 
-            <section>
-                <div className="flex justify-center mt-12">
-                    {numberOfCarts === 3 ? (
-                    <button
-                        className="bg-gray-800 text-white py-2 px-4 rounded"
-                        onClick={() => setNumberOfCarts(4)}
-                    >
-                        View more
-                    </button>
-                    ) : (
-                    <button
-                        className="bg-gray-800 text-white py-2 px-4 rounded"
-                        onClick={() => setNumberOfCarts(3)}
-                    >
-                        View less
-                    </button>
-                    )}
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="p-5">
+      <Description />
+      <Cart numberOfCarts={numberOfCarts} />
+
+      <section>
+        <div className="flex justify-center mt-12">
+          {numberOfCarts === 3 ? (
+            <button
+              className="bg-gray-800 text-white py-2 px-4 rounded"
+              onClick={() => setNumberOfCarts(4)}
+            >
+              View more
+            </button>
+          ) : (
+            <button
+              className="bg-gray-800 text-white py-2 px-4 rounded"
+              onClick={() => setNumberOfCarts(3)}
+            >
+              View less
+            </button>
+          )}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default LandingPage;
